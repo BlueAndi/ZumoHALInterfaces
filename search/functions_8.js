29ddb655312f7b9d3d5bed5c9ebd1f28984a6344
BlueAndi <BlueAndi@users.noreply.github.com>
2025-06-29 11:12:09 +0000

    deploy: bf0d84d523c1bc13e5ea29e5a8046bfbf2f8e724

---
+++ search/functions_8.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['send_0',['send',['../classISender.html#aa0aeb9aed0029fe532f0de741ecb9109',1,'ISender::send(const void *data, size_t size) const =0'],['../classISender.html#aefcfa1e5dfc2f5521c53e6cfea9acc47',1,'ISender::send(const char *str) const =0']]],
+  ['setchannel_1',['setChannel',['../classISender.html#adb2c952b3ddecbb2ff724621f1ea924b',1,'ISender']]],
+  ['setmaxspeed_2',['setMaxSpeed',['../classISettings.html#a9ca6965ca3db42dd74e7d34b95aae320',1,'ISettings']]],
+  ['setspeeds_3',['setSpeeds',['../classIMotors.html#a07f7439daa09e3525e19b99e3b9c7e92',1,'IMotors']]]
+];
---
+++ search/variables_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['valuex_0',['valueX',['../struct__IMUData.html#af5b76d9c4d57d432615f1e0efd0fc3b8',1,'_IMUData']]],
+  ['valuey_1',['valueY',['../struct__IMUData.html#a7581c338f9aa4d65b2530c7b6a0e0253',1,'_IMUData']]],
+  ['valuez_2',['valueZ',['../struct__IMUData.html#a0491a4d61f956b66773776dbcee9d021',1,'_IMUData']]]
+];
