754c8a25b078fc70e6adeaecbcf5c90e1bfd5009
nhjschulz <github@schulznorbert.de>
2025-10-30 11:35:21 +0100

    Add power interface
    Add IPower Interface for reading battery
    voltage and indicate USB power present.
    
    https://github.com/BlueAndi/ZumoHALInterfaces/issues/14

---
+++ include/IPower.h
@@ -0,0 +1,96 @@
+/* MIT License
+ *
+ * Copyright (c) 2023 - 2025 Andreas Merkle <web@blue-andi.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*******************************************************************************
+    DESCRIPTION
+*******************************************************************************/
+/**
+ * @brief  Abstract Power interface
+ * @author Norbert Schulz <github@schulznorbert.de>
+ * 
+ * @addtogroup HALInterfaces
+ *
+ * @{
+ */
+#ifndef IPOWER_H
+#define IPOWER_H
+
+/******************************************************************************
+ * Compile Switches
+ *****************************************************************************/
+
+/******************************************************************************
+ * Includes
+ *****************************************************************************/
+#include <stdint.h>
+
+/******************************************************************************
+ * Macros
+ *****************************************************************************/
+
+/******************************************************************************
+ * Types and Classes
+ *****************************************************************************/
+
+/** The abstract buzzer interface. */
+class IPOWER_H
+{
+public:
+    /**
+     * Destroys the interface.
+     */
+    virtual ~IPower()
+    {
+    }
+
+    /**
+     * Indicate if the robot is powered over USB.
+     * 
+     *  @return true if power is detected on the USB connector, false otherwise.
+     */
+    virtual bool isUsbPowered() = 0;
+
+    /**
+     * Read battery power in millivolts.
+     * 
+     *  @return Actual battery voltage value in millivolts.
+     */
+    virtual uint16_t getBatteryVoltage() = 0;
+
+protected:
+    /**
+     * Constructs the interface.
+     */
+    IPower()
+    {
+    }
+
+private:
+};
+
+/******************************************************************************
+ * Functions
+ *****************************************************************************/
+
+#endif /* IPOWER_H */
+/** @} */
